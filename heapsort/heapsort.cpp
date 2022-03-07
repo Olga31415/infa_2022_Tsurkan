46a001f02eab9b17b50bd4a58fa68fae4cc142f1
Olga31415 <90979692+Olga31415@users.noreply.github.com>
2022-03-07 22:41:46 +0300

    Add files via upload

---
+++ heapsort/heapsort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void heap_trans(int arr[150000], int N, int i){
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+    if (left < N and arr[left] > arr[largest]){
+        largest = left;
+    }
+    if (right < N and arr[right] > arr[largest]){
+        largest = right;
+    }
+    if (largest != i){
+        swap(arr[i], arr[largest]);
+        heap_trans(arr, N, largest);
+    }
+}
+
+void heapsort(int arr[150000], int N){
+    for (int i = N / 2 - 1; i >= 0; i--){
+        heap_trans(arr, N, i);
+    }
+    for (int i=N-1; i>=0; i--){
+        swap(arr[0], arr[i]);
+        heap_trans(arr, i, 0);
+    }
+}
+
+int main(){
+    srand(time(NULL));
+    int N = 150000;
+    int arr[150000]={0};
+    for( int i=0; i<N;++i){
+        arr[i]=rand()%20000+1;
+    }
+
+   heapsort(arr, N);
+
+    /*for(int i = 0;i<N;++i){
+        cout<<arr[i]<<"; ";
+    }*/
+    cout << "runtime = " << clock()/10000.0 << endl; // время работы программы
+    system("pause");
+    return 0;
+}
